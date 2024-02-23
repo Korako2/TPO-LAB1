b1dc12c1788d30580705bdc9ec38ac542a9343ab
Kirill Guryanov <korako2@mail.ru>
2024-02-24 00:48:17 +0300

    feat: add sec function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,7 @@
 import functions.Sec;
-import org.apache.commons.math3.util.CombinatoricsUtils;
-import util.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
         Sec sec = new Sec();
-        MathUtils mathUtils = new MathUtils();
-        System.out.println(mathUtils.calcEulerNumber(2));
     }
 }
--- src/main/java/functions/Sec.java
+++ src/main/java/functions/Sec.java
@@ -1,10 +1,41 @@
 package functions;
 
-
+import util.MathUtils;
 
 public class Sec {
-    public double sec(double x) {
-        return 1 / Math.cos(x);
+    MathUtils mathUtils = new MathUtils();
+
+    public double sec(double x, int n) throws IllegalArgumentException {
+        if (n > 30) throw new IllegalArgumentException("The decomposition order is too large");
+        double sec = 0;
+        x = bringXtoFirstTurnOfCircle(x);
+        if (Math.abs(x) > Math.PI / 2) {
+            if (x > 0) x = Math.PI - x;
+            else x = -Math.PI - x;
+        }
+        double factorial = 1;
+        int t = 1;
+        for (int k = 0; k < n; k++) {
+            if (k != 0) {
+                factorial /= t;
+                factorial /= t + 1;
+                t += 2;
+            }
+            double E = mathUtils.calcEulerNumber(2 * k);
+            sec += Math.pow(-1, k) * E * Math.pow(x, 2 * k) * factorial;
+        }
+        return sec;
     }
+
+
+    private double bringXtoFirstTurnOfCircle(double x) {
+        double doublePI = 2 * Math.PI;
+        while (x > doublePI)
+            x = x - doublePI;
+        while (x < -doublePI)
+            x = x + doublePI;
+        return x;
+    }
+
 
 }
