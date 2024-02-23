06f6a268b62621f4928568b28dbd1e493272ece1
Kirill Guryanov <korako2@mail.ru>
2024-02-23 15:57:29 +0300

    fix: replace data type

--- src/main/java/functions/Sec.java
+++ src/main/java/functions/Sec.java
@@ -1,13 +1,23 @@
 package functions;
 
+import java.math.BigDecimal;
+
 public class Sec {
     public double sec(double x) {
         System.out.println(calcBinomialCoefficient(10,  5));
         return 1 / Math.cos(x);
     }
 
-    private double calcBinomialCoefficient(int n, int k) {
-        return factorial(n) / (factorial(k) * factorial(n - k));
+    private BigDecimal calcBinomialCoefficient(int n, int k) {
+        return calcFactorial(n).divide(calcFactorial(k).multiply(calcFactorial(n - k)));
+
     }
 
+    private BigDecimal calcFactorial(int n) {
+        BigDecimal factorial = BigDecimal.ONE;
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial.multiply(BigDecimal.valueOf(i));
+        }
+        return factorial;
+    }
 }
